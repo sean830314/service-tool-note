c3b0c5b2a442235deec0f4b5c4cb113f51366146
Sean.Chen <sean830314@gmail.com>
2021-04-06 10:34:06 +0800

    add rabbitmq Deduplication

---
+++ RabbitMQ-note/Deduplication/new_task.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+import pika
+import sys
+
+connection = pika.BlockingConnection(
+    pika.ConnectionParameters(host='localhost'))
+channel = connection.channel()
+
+channel.queue_declare(queue='task_queue1', durable=True, arguments={'x-message-deduplication': True})
+
+message = ' '.join(sys.argv[1:]) or "Hello World!"
+channel.basic_publish(
+    exchange='',
+    routing_key='task_queue1',
+    body=message,
+    properties=pika.BasicProperties(
+        delivery_mode=2,  # make message persistent
+        headers={'x-deduplication-header': 'test'}
+    ))
+print(" [x] Sent %r" % message)
+connection.close()
